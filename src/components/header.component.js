c56282449bb5be91365f9e7b34aac55c3143e5ee
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-30 11:41:02 +0300

    feat: add sitting by core component, methods: init, hide, show; and setting app first component heading; addlistenet btn - header.hide

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -4,4 +4,18 @@ export class HeaderComponent extends Component {
     constructor(id) {
         super(id)
     }
+
+    init() {
+        // console.log('$el: ', this.$el);
+
+       const btn = this.$el.querySelector('.js-header-start')
+       console.log('btn: ', btn);
+       btn.addEventListener('click', buttonHandler.bind(this))
+    }
+}
+
+function buttonHandler() {
+    // console.log('this.$el.buttonHandler: ', this.$el);
+    // this.$el.classList.add('hide')
+    this.hide()
 }
