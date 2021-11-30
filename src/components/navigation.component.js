a03ee09e1aad6edacbbe6a63e9905e5c38f5e946
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-30 13:02:08 +0300

    feat: add and link thre component: create, favorite, posts; in navigation.component add method registerTabs, add name to component, show and hide component

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,12 +3,18 @@ import { Component } from "../core/component";
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+
+        this.tabs = []
     }
 
     init() {
         // console.log('navigation: ', this.$el);
         this.$el.addEventListener('click', tabClickHandler.bind(this))
 
+    }
+
+    registerTabs(tabs) {
+        this.tabs = tabs
     }
 
 }
@@ -21,5 +27,12 @@ function tabClickHandler(event) {
        })
         // console.log('event.target: ', event.target);
         event.target.classList.add('active')
+        // console.log('this.tabs',this.tabs);
+
+        const activeTab = this.tabs.find(t => t.name === event.target.dataset.name)
+        // console.log('activeTab: ', activeTab);
+        this.tabs.forEach(t => t.component.hide())
+        activeTab.component.show()
+
     }
 }
