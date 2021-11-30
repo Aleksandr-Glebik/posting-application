23b31429ace0538cb572bcbccc4ff9df5390f4cf
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-30 11:26:16 +0300

    feat: add core component and first component header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // console.log('Started!');
-async function app() {
-    await fetch('setting up babel')
-}
+// async function app() {
+//     await fetch('setting up babel')
+// }
+
+import {HeaderComponent} from './components/header.component'
+
+const header = new HeaderComponent('header')
+console.log('header :', header);
